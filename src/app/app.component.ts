ace3beb49f056635e1b045a4158ac8d1f3a1b37d
Nikita Ivanov <fogelnk777 gmail.com>
2024-11-29 00:29:11 +0300

    registration and authorization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AuthorizationComponent} from './pages/authorization/authorization.component';
+import {RegistrationComponent} from './pages/registration/registration.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AuthorizationComponent, RegistrationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
